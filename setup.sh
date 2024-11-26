c809edb01bf3c74b92d42b345a52f0eb87d19dc8
jjoaom <joaomarcos.aquino@hotmail.com>
2024-11-26 14:39:04 -0300

    cleanup old setup

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Descarta quaisquer alterações locais e força o checkout da branch 'linux'
-git reset --hard
-git checkout linux
-git pull origin linux
-
-echo "Branch linux atualizada com sucesso!"
